812a6182956339cc2f83bf932bf2654013a89426
spidey45-creator <adityapitale04@gmail.com>
2026-07-04 15:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.17%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public: 
+
+        int tcost(int x,vector<int> &cost,vector<int> &dp){
+            if(x==0){return cost[0];}
+            if(x==1){return cost[1];}
+            if(dp[x]!=-1){return dp[x];}
+
+           dp[x]=cost[x]+min(tcost(x-1,cost,dp),tcost(x-2,cost,dp));
+            return dp[x];
+
+        }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n= cost.size();
+        if(cost.size()==1){return cost[0];}
+        vector<int> dp(n,-1);
+        int x=n-1;
+
+        int ans= min(tcost(x,cost,dp),tcost((x-1),cost,dp));
+        return ans;
+      
+        
+    }
+};
