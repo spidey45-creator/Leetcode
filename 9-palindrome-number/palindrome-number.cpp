c4d88674f29eff83ea8ab66818a7e35e8752dc75
spidey45-creator <adityapitale04@gmail.com>
2026-07-25 15:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (65.75%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,9 +5,11 @@ public:
         else{
             int xcopy=x;
             long long reverse=0;
-            while(x>0){ reverse=(reverse*10)+(x%10);
-            x/=10;}
-            return reverse==xcopy;
+            while(xcopy>0)
+            {reverse=(reverse*10)+(xcopy%10);
+            xcopy/=10;}
+            return reverse==x;
+            
 
         }
         
