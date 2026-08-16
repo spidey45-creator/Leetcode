b7c1a625686fba832b388f7b4c17e83b06424c0c
spidey45-creator <adityapitale04@gmail.com>
2026-08-16 20:05:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (83.39%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,38 +1,48 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        // Get number of rows
         int m = matrix.size();
-        // Get number of columns
         int n = matrix[0].size();
+        int col0 = 1;
 
-        // Traverse each cell of the matrix
+        // Step 1: Mark rows and columns in the first row & column
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                // If current cell is zero
                 if (matrix[i][j] == 0) {
-                    // Mark all elements in this row as -1 (except existing zeros)
-                    for (int col = 0; col < n; col++) {
-                        if (matrix[i][col] != 0)
-                            matrix[i][col] = -1e8;
-                    }
-                    // Mark all elements in this column as -1 (except existing zeros)
-                    for (int row = 0; row < m; row++) {
-                        if (matrix[row][j] != 0)
-                            matrix[row][j] = -1e8;
+                    matrix[i][0] = 0; // Mark i-th row
+
+                    if (j != 0) {
+                        matrix[0][j] = 0; // Mark j-th col
+                    } else {
+                        col0 = 0; // Column 0 needs zeroing
                     }
                 }
             }
         }
 
-        // Second pass: replace all -1 markers with 0
-        for (int i = 0; i < m; i++) {
+        // Step 2: Set zeroes for inner matrix (1..m-1, 1..n-1)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != 0) {
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        // Step 3: Handle the first row using matrix[0][0]
+        if (matrix[0][0] == 0) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == -1e8)
-                    matrix[i][j] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Step 4: Handle the first column using col0
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
-        
-    
 };
